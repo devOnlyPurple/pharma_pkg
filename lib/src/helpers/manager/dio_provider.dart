902cd1f0633e22e6bf7416e7448a99e879705b40
devOnlyPurple <davio.sowa9@gmail.com>
2024-08-19 21:15:59 +0000

    xxxc1 commit

--- lib/src/helpers/manager/dio_provider.dart
+++ lib/src/helpers/manager/dio_provider.dart
@@ -1,27 +1,48 @@
 import 'dart:io';
-
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
 import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:pharmacie_pkg/src/helpers/constants/api_constant.dart';
 
 Future<Dio> getDio() async {
   Dio dio = Dio();
-
   dio.options.headers['apiKey'] = API_KEY;
 
-  // Ajout du certificat
-  ByteData cert = await rootBundle.load("assets/cert/kondjigbale.crt");
+  // URL du certificat hébergé en ligne
+  const certificateUrl =
+      'https://github.com/devOnlyPurple/pharma_pkg/raw/main/assets/cert/kondjigbale.crt';
+
+  // Téléchargement du certificat
+  final ByteData certData = await downloadCertificate(certificateUrl);
+
+  // Configuration de Dio avec le certificat
   dio.httpClientAdapter = IOHttpClientAdapter(
     createHttpClient: () {
       final SecurityContext scontext = SecurityContext();
-
-      scontext.useCertificateChainBytes(cert.buffer.asUint8List());
+      scontext.useCertificateChainBytes(certData.buffer.asUint8List());
 
       HttpClient client = HttpClient(context: scontext);
-
       return client;
     },
   );
   return dio;
+}
+
+Future<ByteData> downloadCertificate(String url) async {
+  Dio dio = Dio();
+  final directory =
+      await getTemporaryDirectory(); // Utilisation d'un répertoire temporaire
+  final filePath = '${directory.path}/kondjigbale.crt';
+
+  try {
+    // Téléchargement du fichier
+    await dio.download(url, filePath);
+    final file = File(filePath);
+    final bytes = await file.readAsBytes();
+    return ByteData.sublistView(Uint8List.fromList(bytes));
+  } catch (e) {
+    print('Error downloading certificate: $e');
+    rethrow;
+  }
 }
